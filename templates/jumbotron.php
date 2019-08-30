dceb55427a41fe420b8bef453509cc2a76966a2b
odil-io <github@odil.io>
2019-08-30 13:07:07 +0200

    Papapapap!

--- templates/jumbotron.php
+++ templates/jumbotron.php
@@ -1,7 +1,27 @@
-
-<div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">Hier komt de "header"afbeelding met tekst er in.</p>
-  <hr class="my-4">
-  <p>Dus geen header.php meer daarvoor maken.</p>
-</div>
+<section class="page-header bg-gray-300">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-6 py-md-5">
+        <div class="content py-md-6">
+          <h1>Bruggenbouwers in complexe <br>technische projecten</h1>
+          <p>TPM (Technisch Project Management) zorgt voor overzicht. Voor korte lijnen en een zo snel mogelijke realisatie van ieder technisch project. <a href="/over-tpm">Meer over TPM</a></p>
+          <p><a class="btn btn-white" href="#">Zo kunnen we u helpen</a></p>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 text-center">
+        <img class="img-fluid" src="https://placehold.it/500" alt="TPM Logo met foto invulling">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 text-center">
+        <div class="enumeration">
+          <span>Project management & Consultancy</span>
+          <span class="seperator"></span>
+          <span>Enginering</span>
+          <span class="seperator"></span>
+          <span>Interim professionals</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
